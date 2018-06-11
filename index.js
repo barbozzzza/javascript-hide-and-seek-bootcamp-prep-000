a698ff77de270e024772af76084c84440aa2398b
jose barboza <jrbarboza08@gmail.com>
2018-06-11 00:20:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function getFirstSelector(selector){
+  
+  return document.querySelector(selector)
+  
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target")
+  
+}
+
+function deepestChild(){
+
+ let node = document.getElementById('grand-node')
+ let nextNode = node.children[0]
+ while (nextNode) {
+   node = nextNode
+   nextNode = node.children[0]
+ }
+
+  return node
+  
+  
+  
+}
+
+function increaseRankBy(n){
+  
+  var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+ for (let i = 0; i < lis.length; i++) {
+   lis[i].innerHTML = parseInt(lis[i].innerHTML) + parseInt(n);
+ }
+  
+  
+  
+}
